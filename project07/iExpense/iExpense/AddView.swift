0e63b98db51b31f2608d87857bcb53d47eafb5bb
Filip Soszyński <101994826+xFloly@users.noreply.github.com>
2024-01-12 20:41:17 +0100

    project07 done

---
+++ project07/iExpense/iExpense/AddView.swift
@@ -0,0 +1,55 @@
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    let currencies = [
+        "USD",
+        "EUR",
+        "GBP",
+         "PLN"
+    ]
+    @State private var curr="USD"
+
+    var expenses: Expenses
+
+    let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                Picker("Currency",selection: $curr){
+                    ForEach(currencies, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField("Amount", value: $amount, format: .currency(code: curr))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount,curr: curr)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
---
+++ project07/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
